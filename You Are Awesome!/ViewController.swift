abf89425e6071af0b140a817c55d892d42f21b21
Emily <walker.emily010@gmail.com>
2019-09-03 21:12:17 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Emily Walker on 9/3/19.
+//  Copyright © 2019 Emily Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
